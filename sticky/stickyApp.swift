4342bd0e2e9fb381bedf8452bb7ecef2434c415f
Dmitry <d.myachn@gmail.com>
2023-10-23 11:06:17 +0300

    Initial Commit

---
+++ sticky/stickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickyApp.swift
+//  sticky
+//
+//  Created by MyachinGarpix on 23.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct stickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
