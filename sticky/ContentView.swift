5cf4b6fe1db06b6d2af4e7f9f1203a2220cbb605
Dmitry <d.myachn@gmail.com>
2023-10-23 12:26:52 +0300

    done

--- sticky/ContentView.swift
+++ sticky/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    let centerWidth = UIScreen.main.bounds.width / 2
+    let centerHeight = UIScreen.main.bounds.height / 2
+    
+    @State var position = CGSize()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Canvas { context, size in
+            
+            let circle = context.resolveSymbol(id: 0)!
+            let shape = context.resolveSymbol(id: 1)!
+            
+            context.addFilter(.alphaThreshold(min: 0.6, color: .white))
+            context.addFilter(.blur(radius: 10))
+            
+            context.drawLayer { context in
+                context.draw(circle, at: .init(x: centerWidth, y: centerHeight))
+                context.draw(shape, at: .init(x: centerWidth, y: centerHeight))
+            }
+            
+        } symbols: {
+            Rectangle()
+                .frame(width: 150, height: 150)
+                .tag(0)
+            Circle()
+                .frame(width: 150, height: 150)
+                .tag(1)
+                .offset(position)
         }
-        .padding()
+        .ignoresSafeArea()
+        .gesture(
+            DragGesture()
+                .onChanged({ value in
+            position = value.translation
+        })
+                .onEnded({ _ in
+                    withAnimation {
+                        position = CGSize()
+                    }
+                })
+        )
+        .background(.cyan)
     }
 }
 
